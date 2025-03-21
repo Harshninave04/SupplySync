3ed6827d69ec712a679efc8df84b7c5e6869e72d
Harsh Ninave <harshninave58@gmail.com>
2025-03-21 22:13:57 +0530

    Initialize React app with Vite and Tailwind CSS setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 class="flex items-center justify-center h-screen animate-bounce text-6xl font-bold">Supply Sync</h1>
+    </>
+  );
+}
+
+export default App;
